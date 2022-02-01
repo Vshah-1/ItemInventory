34c06088fbdeb0688c498db3e240b47850c1bf84
Vshah-1 <vshah1@protonmail.com>
2022-02-01 10:24:08 +0000

    2nd Commit: contains code for item inventory application which stores data item data, and is posting onto local host

--- src/main/java/com/qa/ItemInventory/ItemInventoryApplication.java
+++ src/main/java/com/qa/ItemInventory/ItemInventoryApplication.java
@@ -1,8 +1,12 @@
 package com.qa.ItemInventory;
 
+import com.qa.ItemInventory.Item.Item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -12,4 +16,16 @@ public class ItemInventoryApplication {
 		SpringApplication.run(ItemInventoryApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Item> Items() {
+		return List.of(
+				new Item(
+						1L,
+						"Gilded Key",
+						"Allows player to open locked chests")
+		);
+	}
 }
+
+
+
