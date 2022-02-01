1431c92c92249f5f52b5c14f991d419e59732adb
Vshah-1 <vshah1@protonmail.com>
2022-02-01 23:31:24 +0000

    Added repository to ItemService and removed return List.of method

--- src/main/java/com/qa/ItemInventory/Item/ItemService.java
+++ src/main/java/com/qa/ItemInventory/Item/ItemService.java
@@ -1,17 +1,29 @@
 package com.qa.ItemInventory.Item;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
     public List<Item> getItems() {
-        return List.of(
-                new Item(
-                        1L,
-                        "Gilded Key",
-                        "Allows player to open locked chests")
-        );
+        return itemRepository.findAll();
     }
 }
+
+
+//return List.of(
+//        new Item(
+//        1L,
+//        "Gilded Key",
+//        "Allows player to open locked chests")
+//        );
